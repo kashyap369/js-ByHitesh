5753e24fb3f574daa2edf0f1acb792df7f328162
kashyap369 <shubham983346@gmai.com>
2024-08-22 22:01:21 +0530

    rename again

--- basics/01_variable.js
+++ basics/01_variable.js
@@ -1,8 +1,3 @@
-/*
- try to avoid var instead of that use let 
-
-*/
-
 let a =4;
 let b=3;
 let c = a+b;
@@ -11,4 +6,3 @@ const name = "shubham kashyap";
 var city = "mumbai"
 age = 22;
 console.table([name,city,age]);
-
