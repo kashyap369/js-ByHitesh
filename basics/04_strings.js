e0724d87554eabf24efe3ff93171d1bd35c69ec3
kashyap369 <shubham983346@gmai.com>
2024-08-24 01:06:54 +0530

    adding 04 file strings

---
+++ basics/04_strings.js
@@ -0,0 +1,9 @@
+const name = "shubham";
+const surname = "kashyap";
+// console.log(name + surname + "form");
+// console.log(`hellow my name is ${name} and my surname is ${surname}`);
+// console.log(typeof (name));
+const mystring = new String("shubham");
+// console.log(mystring.indexOf('u'));
+const newstring = mystring.substring(0,5);
+console.log(newstring);
