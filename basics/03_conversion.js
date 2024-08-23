07287df555d09b0d735a92c620d677c3e0223f99
kashyap369 <shubham983346@gmai.com>
2024-08-24 00:13:31 +0530

    ading info about head nad stack

--- basics/03_conversion.js
+++ basics/03_conversion.js
@@ -2,3 +2,13 @@ let score = "22";
 console.log(typeof(score));
 let scoreInNumber = Number(score);
 console.log(typeof(scoreInNumber));
+
+
+
+/*
+
+what are stack and heap memory ?
+when when you use primitive datatype then stack memory is used 
+and when you use non primitive datatype then heap memory is used 
+
+*/
