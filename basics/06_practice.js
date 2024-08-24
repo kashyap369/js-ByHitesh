0fea97641e8347aa3a99315876f5946b5ac2dad6
kashyap369 <shubham983346@gmai.com>
2024-08-24 07:46:38 +0530

    adding 06

---
+++ basics/06_practice.js
@@ -0,0 +1,9 @@
+let mymessage = 'this is ';
+let mys2 = 'message';
+let fullmsg = mymessage + '' +mys2;
+
+// console.log(fullmsg);
+let jsstring = 'jAVascript';
+console.log(jsstring.toLowerCase());
+
+
